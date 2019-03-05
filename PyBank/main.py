94bb750c6d37461c272bd66bb534d79ffa81f06b
Gunjan <nidhi@Nidhis-Air.attlocal.net>
2019-03-04 20:39:56 -0800

    output achieved

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,6 +1,7 @@
 import os
 import csv
 
+
 #set the path
 
 budgetcsvpath = os.path.join(".", "Resources", "budget_data.csv")
@@ -14,7 +15,7 @@ with open (budgetcsvpath,newline="") as budgetfile:
 
     budgetheader = next(budgetreader)
 
-    print(f"budget header : {budgetheader}")
+    #print(f"budget header : {budgetheader}")
 
     count = 0 #to count the number of months
     Net_amount = 0
@@ -37,10 +38,9 @@ with open (budgetcsvpath,newline="") as budgetfile:
         
         if index ==0:
             index_tracker = 0
-            #difference = int(row[1]) - int(profitloss[index_tracker]) # for jan - 867884-867884 index[0]
+        
             difference = 0
-            #profit_tracker.append(difference) #[0,116771,]
-            #month_tracker.append(row[0]) #[jan-10, feb-10, mar-10, apr-10,...]
+           
 
             index += 1
         else:
@@ -51,24 +51,29 @@ with open (budgetcsvpath,newline="") as budgetfile:
             index += 1 #2
 
 
-    print(f"profit_tracker , Month  :  {profit_tracker} ,  {month_tracker}")
+    #print(f"profit_tracker , Month  :  {profit_tracker} ,  {month_tracker}")
        
+    avg_change = round((sum(profit_tracker)/float(len(profit_tracker))),2)
+    min_value = min(profit_tracker)
+    min_valueindex = int(profit_tracker.index(min_value)) # get index of min value
+    min_valuemonth = month_tracker[min_valueindex] #get month with min value change index
+    
+    max_value = max(profit_tracker) 
+    max_valueindex = int(profit_tracker.index(max_value))#get index of max change
+    max_valuemonth = month_tracker[max_valueindex] #get max value change month
 
     # loop for comparing the value of profit change 
+    print(f"  Financial Analysis ")
+    print(f"-------------------------------------")
+    print(f"  Total Months : {count}")
+    print(f"  Total Amount : {Net_amount}")
+    print(f"  Average Change : {avg_change}")
+    print(f"  Greatest increase in Profits  : {max_valuemonth}    (${max_value})")
+    print(f"  Greatest decrease in Profits  : {min_valuemonth}    (${min_value})")
 
-    print(f" Total Months : {count}")
-    print(f" Total Amount : {Net_amount}")
-
-    #for index in profit_tracker:
-
-       # profit = profit_tracker[index]
-       # month =  month_tracker[index]
-       # print(profit + "   " + month)
+   
 
 
-    #print({f" difference : {")
-    #print(f" Greatest Increase in Profits :    ( {row[3]})")
-    #print(f" Greatest Decrease in Profits :    ( {row[3]})")
 
 
 
